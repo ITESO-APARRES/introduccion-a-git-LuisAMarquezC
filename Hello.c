2557badab2c7f905bcba73ef62dda5a1364fb68e
LuisAMarquezC <115443424+LuisAMarquezC@users.noreply.github.com>
2022-10-10 13:08:01 -0500

    Comentario desde Git

--- Hello.c
+++ Hello.c
@@ -1,9 +1,12 @@
+/*
+    Se añadeio este comentario desde git
+*/
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
     printf("Hola Mundo\n");
     printf("Esta linea fue modificada desde git\n");
-    printf("Ahora modificamos esta linea en git para comparar con el repositorio local");
     return 0;
 }
