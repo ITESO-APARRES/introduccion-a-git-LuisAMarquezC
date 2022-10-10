5f38f5838f7a183d68dfcf3bc0432b92d3390ea4
LuisAMarquezC <115443424+LuisAMarquezC@users.noreply.github.com>
2022-10-10 12:55:09 -0500

    Se añade una tercera línea

--- Hello.c
+++ Hello.c
@@ -4,5 +4,6 @@ int main(int argc, char const *argv[])
 {
     printf("Hola Mundo\n");
     printf("Esta linea fue modificada desde git\n");
+    printf("Ahora modificamos esta linea en git para comparar con el repositorio local");
     return 0;
 }
